888c865c4e1e09891a4b21e69bed6967ba7070f7
Tobias Fischer <tobias.fischer@gbv.de>
2026-04-14 14:01:32 +0000

    add custom expires_at config

--- src/updater/LocUpdater.js
+++ src/updater/LocUpdater.js
@@ -59,6 +59,15 @@ function isInTimeRange(currentHour, fromHour, toHour) {
   }
 }
 
+function getNewCustomExpiresAt() {
+  const newExpiresAt = new Date()
+  const customExpirationConfig = info?.config?.plugin?.['custom-data-type-loc']?.config?.update_loc?.custom_expires_days || 1
+
+  newExpiresAt.setDate(newExpiresAt.getDate() + customExpirationConfig);
+
+  return newExpiresAt.toISOString()
+}
+
 function uriToSuggestUrl(uri) {
   const uriParts = uri.split('/');
   const path = uriParts.slice(3, -1).join('/');
@@ -181,7 +190,11 @@ main = (payload) => {
               newCdata._standard = LocUtil.getStandardFromLocJSON(null, resultObject, newCdata, databaseLanguages);
               if (hasChanges(payload.objects[index].data, newCdata)) {
                 payload.objects[index].data = newCdata;
-              } else { }
+              } else {
+                payload.objects[index].data = originalCdata
+              }
+              // set expires at for the custom data object according to the plugin base config
+              payload.objects[index].data._expires_at = getNewCustomExpiresAt()
             }
           } else {
             console.error('No matching record found');
